749385f94628ebb9d151c806b43c17609c7e61bd
Marcin Szwarc <marcinszwarc@hotmail.com>
2024-12-19 13:20:50 +0100

    Solved task 2

--- main.py
+++ main.py
@@ -37,3 +37,40 @@ class AccountManager:
     def viewAccounts():
         for acc in AccountManager.__accounts:
             acc.displaySummary()
+
+
+#
+# -----------------------------------------------------------------------------
+#
+
+class ToDoList:
+
+    def __init__(self):
+        self.tasks = []
+    
+    def add_task(self, title: str, description: str, priority: int) -> None:
+        self.tasks.append(Task(title, description, priority, False))
+
+    def remove_taks(self, title: str) -> None:
+        for i, task in enumerate(self.tasks):
+            if task.title == title:
+                del self.tasks[i]
+    
+    def view_sorted_tasks(self) -> None:
+        for task in self.tasks:
+            print(task.title, f'(priority: {task.priority})')
+            print('Completed' if task.is_completed else 'Not completed')
+            print(task.description)
+
+    def mark_as_completed(self, title) -> None:
+        for task in self.tasks:
+            if task.title == title:
+                task.is_completed = True
+
+class Task:
+    
+    def __init__(self, title: str, description: str, priority: int, is_completed: bool):
+        self.title = title
+        self.description = description
+        self.priority = priority
+        self.is_completed = is_completed
